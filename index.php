bbf11a6fcd1fcf992ad0a34d2dc8eafeb1005599
Michael Josephson <michael.josephson@zestia.com>
2016-11-15 21:55:39 +0000

    add style for activity-generated lists

--- index.php
+++ index.php
@@ -24,6 +24,10 @@
     <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
     <script src="js/ie-emulation-modes-warning.js"></script>
 
+    <style>
+      .activity-items { padding-left:0; list-style:none; }
+    </style>
+
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
